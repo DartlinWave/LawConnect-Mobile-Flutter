123215ca0456a51c6649d0879cc099d6c734a891
Camila <u202110385@upc.edu.pe>
2025-05-31 01:51:45 -0500

    feat: add color palette

---
+++ lib/color_palette.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+class ColorPalette {
+  static Color primaryColor = const Color(0xFF191919);
+  static Color secondaryColor = const Color(0xFFEF524F);
+
+  // Button colors
+  static Color mainButtonColor = const Color(0xFF393939);
+  static Color matchButtonColor = const Color(0xFF0CAD17);
+  static Color declineButtonColor = const Color(0xFFD81515);
+  static Color followUpButtonColor = const Color(0xFF6B6B6B);
+
+  // Status colors
+  static Color openColor = const Color(0xFF4F54DA);
+  static Color inEvaluationColor = const Color(0xFFC8CD1A);
+  static Color acceptedColor = const Color(0xFF4FDA4F);
+  static Color closedColor = const Color(0xFFFF0000);
+
+  // Basic Colors
+  static Color whiteColor = const Color(0xFFFFFFFF);
+  static Color blackColor = const Color(0xFF000000);
+  static Color greyColor = const Color(0xFF939393);
+
+}
