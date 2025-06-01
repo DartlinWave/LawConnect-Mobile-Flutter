e7541b78ba8ed6f70e55b483c9483aafbec52d18
Camila <u202110385@upc.edu.pe>
2025-05-31 23:30:05 -0500

    feat: add custom button.

---
+++ lib/basic_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:lawconnect_mobile_flutter/color_palette.dart';
+
+class BasicButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final double width;
+  final double height;
+  final Color backgroundColor;
+
+  const BasicButton(
+    {super.key, 
+    required this.text, 
+    required this.onPressed,
+    required this.width,
+    required this.height,
+    required this.backgroundColor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+
+      child: FilledButton(
+        onPressed: onPressed, 
+        style: FilledButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          textStyle: TextStyle(
+            fontSize: 16,
+            color: ColorPalette.whiteColor,
+          ),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
