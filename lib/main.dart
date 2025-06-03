bb6c4120e9664c18e7c05a203aef819f94b633bf
Camila <u202110385@upc.edu.pe>
2025-06-03 17:48:20 -0500

    feat: add font family to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'LawConnect Mobile',
       debugShowCheckedModeBanner: false,
-      home: MainPage()
+      home: MainPage(),
+      theme: ThemeData(
+        fontFamily: 'PlusJakartaSans',
+      ),
     );
   }
 }
