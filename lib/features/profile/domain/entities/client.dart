e22752acb10fdc4cc7b1cc9dedf8259cd5f55f8d
Camila <u202110385@upc.edu.pe>
2025-06-05 23:48:56 -0500

    feat: add entity for client to mock data

---
+++ lib/features/profile/domain/entities/client.dart
@@ -0,0 +1,15 @@
+class Client {
+  final String id;
+  final String name;
+  final String lastName;
+  final String dni;
+  final String username;
+
+  Client({
+    required this.id,
+    required this.name,
+    required this.lastName,
+    required this.dni,
+    required this.username,
+  });
+}
