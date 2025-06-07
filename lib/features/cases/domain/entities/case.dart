ac9bcfaa6e94a90249141300da2d3ec8d6acf235
Camila <u202110385@upc.edu.pe>
2025-06-07 17:04:19 -0500

    feat: add case entity (includes applicant)

--- lib/features/cases/domain/entities/case.dart
+++ lib/features/cases/domain/entities/case.dart
@@ -1,20 +1,48 @@
 class Case {
-  final String id;
-  final String clientId;
-  final String lawyerId;
+  final int id;
+  final int clientId;
   final String title;
   final String description;
+  final String specialty;
+  final String status;
+  final String image;
   final DateTime createdAt;
   final DateTime updatedAt;
+  final List<Applicant> applicants;
 
   Case({
     required this.id,
     required this.clientId,
-    required this.lawyerId,
     required this.title,
     required this.description,
+    required this.specialty,
+    required this.status,
+    required this.image,
     required this.createdAt,
     required this.updatedAt,
+    required this.applicants,
   });
 
+}
+
+class Applicant {
+  final int id;
+  final int lawyerId;
+  final String name;
+  final String specialty;
+  final double rating;
+  final String image;
+  final String applicationStatus;
+  final DateTime appliedAt;
+
+  Applicant({
+    required this.id,
+    required this.lawyerId,
+    required this.name,
+    required this.specialty,
+    required this.rating,
+    required this.image,
+    required this.applicationStatus,
+    required this.appliedAt,
+  });
 }
