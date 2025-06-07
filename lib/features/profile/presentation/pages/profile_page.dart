29fe1cfdf37b515b82bb3d6721b0de4c5833ab76
Camila <u202110385@upc.edu.pe>
2025-06-07 01:03:35 -0500

    feat: add profile page and data view

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lawconnect_mobile_flutter/core/theme/color_palette.dart';
 import 'package:lawconnect_mobile_flutter/features/profile/domain/entities/client.dart';
+import 'package:lawconnect_mobile_flutter/features/profile/presentation/views/profile_data_view.dart';
+import 'package:lawconnect_mobile_flutter/shared/custom_widgets/basic_button.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -9,20 +12,54 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  final List<Client> _clients = [
+  final List<Client> _client = [
     Client(
-      id: '1', 
-      name: 'John', 
-      lastName: 'Doe', 
-      dni: '12345678', 
-      username: 'johndoe', 
-      image: 'https://ibb.co/LXjJRDrz'
-      )
+      id: '1',
+      name: 'Jane',
+      lastName: 'Doe',
+      dni: '12345678',
+      username: 'janedoe12',
+      image:
+          'https://economia3.com/wp-content/uploads/2019/12/Natalia-Juarranz-EQUIPO-HUMANO-450x450.jpg',
+    ),
   ];
-
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorPalette.whiteColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+
+                ProfileDataView(clients: _client),
+
+                SizedBox(height: 24),
+
+
+                
+                SizedBox(height: 24),
+
+
+                BasicButton(
+                  text: 'Sign Out',
+                  onPressed: () {
+                    // Acción al presionar el botón
+                  },
+                  width: 214,
+                  height: 61,
+                  backgroundColor: ColorPalette.mainButtonColor,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
