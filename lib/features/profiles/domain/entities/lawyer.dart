93a63e6ee8812d6d48d8e734664f678822ae8afe
Camila <u202110385@upc.edu.pe>
2025-06-09 13:11:18 -0500

    feat: add lawyer entity

---
+++ lib/features/profiles/domain/entities/lawyer.dart
@@ -0,0 +1,25 @@
+class Lawyer {
+  final int id;
+  final int userId;
+  final String name;
+  final String lastName;
+  final String dni;
+  final String phone;
+  final String description;
+  final String specialty;
+  final String image;
+  final double rating;
+
+  Lawyer({
+    required this.id,
+    required this.userId,
+    required this.name,
+    required this.lastName,
+    required this.dni,
+    required this.phone,
+    required this.description,
+    required this.specialty,
+    required this.image,
+    required this.rating,
+  });
+}
