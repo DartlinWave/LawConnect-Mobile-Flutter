7a31dd379923416c890edf72eff6f74a909c3f25
Camila <u202110385@upc.edu.pe>
2025-06-16 20:32:07 -0500

    feat: add invitation entity

---
+++ lib/features/cases/domain/entities/invitation.dart
@@ -0,0 +1,13 @@
+class Invitation {
+  final String id;
+  final String caseId;
+  final String lawyerId;
+  final String status;
+
+  Invitation({
+    required this.id,
+    required this.caseId,
+    required this.lawyerId,
+    required this.status,
+  });
+}
