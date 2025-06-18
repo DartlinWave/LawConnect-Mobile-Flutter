878cf9e86b818a9d4bfa4c952b67cf5e9f41a75c
Camila <u202110385@upc.edu.pe>
2025-06-18 17:01:13 -0500

    refactor: add id to update case

--- lib/features/cases/data/models/case_request_dto.dart
+++ lib/features/cases/data/models/case_request_dto.dart
@@ -1,14 +1,17 @@
 class CaseRequestDto {
+  final String caseId;
   final String status;
   final String updatedAt;
 
   const CaseRequestDto({
+    required this.caseId,
     required this.status,
     required this.updatedAt,
   });
 
   Map<String, dynamic> toJson() {
     return {
+      'caseId': caseId,
       'status': status,
       'updatedAt': updatedAt,
     };
