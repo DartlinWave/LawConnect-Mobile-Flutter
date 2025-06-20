0b053e0f1dcd1e5afbd66df3e403d1e7e48b2e8a
Camila <u202110385@upc.edu.pe>
2025-06-19 20:39:41 -0500

    refactor: adapt the list view according to the changes on the entities and addition of DTO, services, bloc

--- lib/features/cases/presentation/views/case_list_view.dart
+++ lib/features/cases/presentation/views/case_list_view.dart
@@ -1,48 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:lawconnect_mobile_flutter/features/cases/domain/entities/case.dart';
-import 'package:lawconnect_mobile_flutter/features/cases/presentation/pages/follow_up_accepted_page.dart';
-import 'package:lawconnect_mobile_flutter/features/cases/presentation/pages/follow_up_closed_page.dart';
-import 'package:lawconnect_mobile_flutter/features/cases/presentation/pages/follow_up_in_evaluation_page.dart';
-import 'package:lawconnect_mobile_flutter/features/cases/presentation/pages/follow_up_open_page.dart';
+import 'package:lawconnect_mobile_flutter/features/cases/presentation/pages/follow_up_page.dart';
 import 'package:lawconnect_mobile_flutter/features/cases/presentation/views/case_card_view.dart';
 import 'package:lawconnect_mobile_flutter/features/profiles/domain/entities/client.dart';
-import 'package:lawconnect_mobile_flutter/features/profiles/domain/entities/lawyer.dart';
 
 class CaseListView extends StatelessWidget {
-  const CaseListView({super.key, required this.cases, required this.client, required this.allLawyers});
+  const CaseListView({super.key, required this.cases, required this.client});
 
   final List<Case> cases;
   final Client client;
-  final List<Lawyer> allLawyers;
 
 void _navigateToCaseFollowUp(BuildContext context, Case caseEntity) {
-    Widget page;
-
-    final Lawyer? _selectedLawyer = allLawyers.firstWhere(
-      (l) => l.id == caseEntity.lawyerId,
-      orElse: () => Lawyer(id: 0, userId: 0, name: '', lastName: '', dni: '', phone: '', description: '', specialty: '', image: '', rating: 0.0),
-    );
-
-    switch (caseEntity.status) {
-      case "OPEN_STATUS":
-        page = FollowUpOpenPage(caseEntity: caseEntity, client: client);
-        break;
-      case "IN_EVALUATION_STATUS":
-        page = FollowUpInEvaluationPage(caseEntity: caseEntity, client: client);
-        break;
-      case "ACCEPTED_STATUS":
-        page = FollowUpAcceptedPage(caseEntity: caseEntity, client: client, lawyer: _selectedLawyer!);
-        break;
-      case "CLOSED_STATUS":
-        page = FollowUpClosedPage(caseEntity: caseEntity, client: client, lawyer: _selectedLawyer!);
-        break;
-      default:
-        throw Exception("Unknown case status: ${caseEntity.status}");
-    }
 
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => page),
+      MaterialPageRoute(builder: (context) => FollowUpPage(
+        chosenCase: caseEntity,
+        client: client,
+      )),
     );
   }
 
