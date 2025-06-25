717173a497607287bb7296118bd31c7a1f6a2f7f
Camila <u202110385@upc.edu.pe>
2025-06-25 16:55:35 -0500

    refactor: add the bloc for case and auth (to get username)

--- lib/features/cases/presentation/pages/follow_up_page.dart
+++ lib/features/cases/presentation/pages/follow_up_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lawconnect_mobile_flutter/core/theme/color_palette.dart';
+import 'package:lawconnect_mobile_flutter/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:lawconnect_mobile_flutter/features/auth/presentation/bloc/auth_state.dart';
 import 'package:lawconnect_mobile_flutter/features/cases/domain/entities/case.dart';
-import 'package:lawconnect_mobile_flutter/features/profiles/domain/entities/client.dart';
+import 'package:lawconnect_mobile_flutter/features/cases/presentation/blocs/case_bloc.dart';
+import 'package:lawconnect_mobile_flutter/features/cases/presentation/blocs/case_event.dart';
+import 'package:lawconnect_mobile_flutter/features/cases/presentation/views/summary_view.dart';
+import 'package:lawconnect_mobile_flutter/shared/custom_widgets/basic_app_bar.dart';
 
 class FollowUpPage extends StatefulWidget {
   const FollowUpPage({super.key, required this.chosenCase});
@@ -12,28 +19,38 @@ class FollowUpPage extends StatefulWidget {
 }
 
 class _FollowUpPageState extends State<FollowUpPage> {
+void _navigateToFullCase() {
+  Navigator.pushNamed(context, '/path-to-insert', arguments: widget.chosenCase);
+}
+
+late final String clientId;
+
+  @override
+  void initState() {
+    super.initState();
+      context.read<CaseBloc>().add(GetCaseDetailsEvent(caseId: widget.chosenCase.id));
+  }
+
   @override
   Widget build(BuildContext context) {
+    final authState = context.watch<AuthBloc>().state;
+    final clientUsername = authState is SuccessAuthState
+        ? authState.user.username
+        : "Unknown User";
+
     return Scaffold(
+      backgroundColor: ColorPalette.whiteColor,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "Follow Up",
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 24,
-                ),
-              ),
+              BasicAppBar(title: clientUsername),
+
               SizedBox(height: 16),
-              Text(
-                "Case Title: ${widget.chosenCase.title}",
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 8),
+              
+              SummaryView(caseEntity: widget.chosenCase, onShowFullCase: _navigateToFullCase)
             ],
           ),
         ),
