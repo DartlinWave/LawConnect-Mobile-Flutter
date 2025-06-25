d54d8036cd6d67b237e47e209c1390b9d60f7b39
Camila <u202110385@upc.edu.pe>
2025-06-25 16:58:45 -0500

    refactor: add space between the content and divider, add a text in case there's an error fetching the data

--- lib/features/cases/presentation/views/summary_view.dart
+++ lib/features/cases/presentation/views/summary_view.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lawconnect_mobile_flutter/core/theme/color_palette.dart';
 import 'package:lawconnect_mobile_flutter/features/cases/domain/entities/case.dart';
-import 'package:lawconnect_mobile_flutter/features/profiles/domain/entities/lawyer.dart';
+import 'package:lawconnect_mobile_flutter/features/cases/presentation/blocs/case_bloc.dart';
+import 'package:lawconnect_mobile_flutter/features/cases/presentation/blocs/case_state.dart';
 
 class SummaryView extends StatelessWidget {
   const SummaryView({
     super.key,
     required this.caseEntity,
-    required this.lawyer,
     required this.onShowFullCase,
   });
 
   final Case caseEntity;
-  final Lawyer lawyer;
   final VoidCallback onShowFullCase;
 
   @override
@@ -31,55 +31,65 @@ class SummaryView extends StatelessWidget {
       CaseStatus.CLOSED: ColorPalette.closedColor,
     };
 
-    return Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Summary",
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 16,
-              color: ColorPalette.blackColor,
-            ),
-          ),
-          SizedBox(height: 4),
-          Row(
+    return BlocBuilder<CaseBloc, CaseState>(builder: (context, state) {
+      if (state is LoadedCaseDetailsState) {
+        final caseEntity = state.caseEntity;
+        return Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text.rich(
-                TextSpan(
-                  text: "${caseEntity.title} – State: ",
-                  style: TextStyle(color: ColorPalette.blackColor),
-                  children: [
+              Text(
+                "Summary",
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                  color: ColorPalette.blackColor,
+                ),
+              ),
+              SizedBox(height: 4),
+        
+              
+              Row(
+                children: [
+                  Text.rich(
                     TextSpan(
-                      text: statusLabel[caseEntity.status] ?? "Unknown",
-                      style: TextStyle(
-                        color:
-                            statusColor[caseEntity.status] ??
-                            ColorPalette.blackColor,
-                      ),
+                      text: "${caseEntity.title} – State: ",
+                      style: TextStyle(color: ColorPalette.blackColor),
+                      children: [
+                        TextSpan(
+                          text: statusLabel[caseEntity.status] ?? "Unknown",
+                          style: TextStyle(
+                            color:
+                                statusColor[caseEntity.status] ??
+                                ColorPalette.blackColor,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  Spacer(),
+        
+                  TextButton(
+                    onPressed: onShowFullCase,
+                    style: TextButton.styleFrom(
+                      backgroundColor: ColorPalette.lighterButtonColor,
+                      foregroundColor: ColorPalette.blackColor,
+                    ),
+                    child: Text("Show Full Case"),
+                  ),
+                ],
               ),
-              Spacer(),
-
-              TextButton(
-                onPressed: onShowFullCase,
-                style: TextButton.styleFrom(
-                  backgroundColor: ColorPalette.lighterButtonColor,
-                  foregroundColor: ColorPalette.blackColor,
-                ),
-                child: Text("Show Full Case"),
-              ),
+              
+              SizedBox(height: 18),
+              Divider(color: ColorPalette.blackColor, height: 1),
             ],
           ),
-          SizedBox(height: 6),
-          SizedBox(height: 6),
-          Divider(color: ColorPalette.blackColor, height: 1),
-        ],
-      ),
-    );
+        );
+      }
+      return const Text("Error loading case summary");
+    });
+
+    
   }
 }
