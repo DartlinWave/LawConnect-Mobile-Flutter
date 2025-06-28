552687f9cedd329e0f4bbcefe56f3f7bfeddddc9
Camila <u202110385@upc.edu.pe>
2025-06-28 13:54:55 -0500

    refactor: make the font subtitle bigger

--- lib/features/cases/presentation/views/timeline_view.dart
+++ lib/features/cases/presentation/views/timeline_view.dart
@@ -22,7 +22,7 @@ class TimelineView extends StatelessWidget {
             "Timeline",
             style: TextStyle(
               fontWeight: FontWeight.w500,
-              fontSize: 15,
+              fontSize: 16,
               color: ColorPalette.blackColor,
             ),
           ),
