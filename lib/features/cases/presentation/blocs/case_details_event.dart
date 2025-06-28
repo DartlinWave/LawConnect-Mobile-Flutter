917e074748a99b329c8a4355d059ea8af1771032
Camila <u202110385@upc.edu.pe>
2025-06-28 01:02:38 -0500

    feat: create case details events for a specific case

---
+++ lib/features/cases/presentation/blocs/case_details_event.dart
@@ -0,0 +1,33 @@
+abstract class CaseDetailsEvent {
+  const CaseDetailsEvent();
+}
+
+class GetCaseDetailsEvent extends CaseDetailsEvent {
+  final String caseId;
+
+  const GetCaseDetailsEvent({required this.caseId});
+}
+
+class CreateCommentEvent extends CaseDetailsEvent {
+  final String caseId;
+  final String authorId;
+  final String type;
+  final String comment;
+
+  const CreateCommentEvent({required this.caseId, required this.authorId, required this.type, required this.comment});
+}
+
+class UpdateCaseStatusEvent extends CaseDetailsEvent {
+  final String caseId;
+  final String status;
+
+  const UpdateCaseStatusEvent({required this.caseId, required this.status});
+}
+
+class FinishCaseEvent extends CaseDetailsEvent {
+  final String caseId;
+  final String status;
+  final String comment;
+
+  const FinishCaseEvent({required this.caseId, required this.status, required this.comment});
+}
