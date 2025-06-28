7fb55b3d431c57f3e177078aef25a8f749a35700
Camila <u202110385@upc.edu.pe>
2025-06-28 01:02:19 -0500

    refactor: delete exact events for a specific case

--- lib/features/cases/presentation/blocs/case_event.dart
+++ lib/features/cases/presentation/blocs/case_event.dart
@@ -7,33 +7,3 @@ class GetCasesEvent extends CaseEvent {
 
   const GetCasesEvent({required this.clientId});
 }
-
-class GetCaseDetailsEvent extends CaseEvent {
-  final String caseId;
-
-  const GetCaseDetailsEvent({required this.caseId});
-}
-
-class CreateCommentEvent extends CaseEvent {
-  final String caseId;
-  final String authorId;
-  final String type;
-  final String comment;
-
-  const CreateCommentEvent({required this.caseId, required this.authorId, required this.type, required this.comment});
-}
-
-class UpdateCaseStatusEvent extends CaseEvent {
-  final String caseId;
-  final String status;
-
-  const UpdateCaseStatusEvent({required this.caseId, required this.status});
-}
-
-class FinishCaseEvent extends CaseEvent {
-  final String caseId;
-  final String status;
-  final String comment;
-
-  const FinishCaseEvent({required this.caseId, required this.status, required this.comment});
-}
