a46ea71a0ff5284828f356d79dc860f65dc5e238
Camila <u202110385@upc.edu.pe>
2025-06-29 13:02:38 -0500

    feat: add profile bloc

---
+++ lib/features/profiles/presentation/bloc/profile_bloc.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lawconnect_mobile_flutter/features/profiles/data/datasources/profile_service.dart';
+import 'package:lawconnect_mobile_flutter/features/profiles/presentation/bloc/profile_event.dart';
+import 'package:lawconnect_mobile_flutter/features/profiles/presentation/bloc/profile_state.dart';
+
+class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
+  ProfileBloc() : super(InitialProfileState()) {
+    on<GetClientProfileEvent>(_onGetClientProfile);
+  }
+
+  Future<void> _onGetClientProfile(GetClientProfileEvent event, Emitter<ProfileState> emit) async {
+    emit(LoadingProfileState());
+
+    try {
+      final client = await ProfileService().fetchClientById(event.clientId);
+      emit(LoadedClientProfileState(client: client));
+    } catch (e) {
+      emit(ErrorProfileState(message: e.toString()));
+    }
+  }
+}
