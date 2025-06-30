13919bad35549469db6e37cee30987a85b0e723f
Camila <u202110385@upc.edu.pe>
2025-06-29 23:12:36 -0500

    refactor: add bloc of profiles

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lawconnect_mobile_flutter/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:lawconnect_mobile_flutter/features/auth/presentation/pages/login_page.dart';
 import 'package:lawconnect_mobile_flutter/features/cases/presentation/blocs/case_bloc.dart';
+import 'package:lawconnect_mobile_flutter/features/profiles/presentation/bloc/profile_bloc.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,6 +18,7 @@ class MainApp extends StatelessWidget {
       providers: [
         BlocProvider<AuthBloc>(create: (context) => AuthBloc()),
         BlocProvider<CaseBloc>(create: (context) => CaseBloc()),
+        BlocProvider<ProfileBloc>(create: (context) => ProfileBloc()),
       ],
       child: MaterialApp(
         title: 'LawConnect Mobile',
